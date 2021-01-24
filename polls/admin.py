f84b2c636afe453b23865e2583e68e2ac1c86fc2
noel-jose <noeljosetp@gmail.com>
2021-01-24 21:53:34 +0530

    Created a polls app

---
+++ polls/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Question,Choice
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Question',        {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], }),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
